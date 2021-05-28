acecf1aa65aa199d3c2ea3d007b16c456aaf1be6
Jefferson00 <jeffersonts00@gmail.com>
2021-05-28 16:41:02 -0300

    create update avatar service

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,16 +4,26 @@ import '@modules/users/providers'
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 import IMailProvaider from './providers/MailProvider/models/IMailProvider';
 import EtherealMailProvider from './providers/MailProvider/implementations/EtherealMailProvider';
+
 import IMailTemplateProvaider from './providers/MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IStorageProvider from './providers/StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from './providers/StorageProvider/implementations/DiskStorageProvider';
 
 container.registerSingleton<IUsersRepository>(
     'UsersRepository',
     UsersRepository,
+);
+
+container.registerSingleton<IStorageProvider>(
+    'StorageProvider',
+    DiskStorageProvider,
 );
 
 container.registerSingleton<IUserTokensRepository>(
